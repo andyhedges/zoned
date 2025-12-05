be9019cf5a09b8c0404590aa9ff73d49abe8dae9
Andy Hedges <andy@hedges.net>
2025-12-05 15:44:04 +0000

    feat: trying to get a decent build

--- app/src/main/java/io/hedges/zoned/app/DefaultDnsRequestRouter.java
+++ app/src/main/java/io/hedges/zoned/app/DefaultDnsRequestRouter.java
@@ -1,24 +1,47 @@
 package io.hedges.zoned.app;
 
+import io.hedges.zoned.core.DnsClient;
 import io.hedges.zoned.core.DnsRequestContext;
 import io.hedges.zoned.core.DnsRequestRouter;
 import io.hedges.zoned.core.dom.DnsHeaderDom;
 import io.hedges.zoned.core.dom.DnsMessageDom;
+import io.hedges.zoned.core.dom.DnsNameDom;
+import io.hedges.zoned.core.dom.DnsResourceRecordDom;
+import lombok.AllArgsConstructor;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+@AllArgsConstructor
 public class DefaultDnsRequestRouter implements DnsRequestRouter {
+
+    private DnsClient client;
 
     @Override
     public void handle(DnsRequestContext ctx) {
+        String query = ctx.query().questions().getFirst().name().toFqdn();
 
-        ctx.response(
-                DnsMessageDom
-                        .builder()
-                        .header(
-                                DnsHeaderDom
-                                        .builder()
-                                        .id(ctx.query().header().id())
-                                        .build()
-                        )
-                        .build());
+        List<DnsResourceRecordDom> answers = new ArrayList<>();
+        client.lookup(query).whenComplete((addresses, t) -> {
+
+            addresses.forEach(a -> {
+                DnsResourceRecordDom dom = DnsResourceRecordDom.builder().name(DnsNameDom.builder().labels(Arrays.stream(a.getHostName().split("\\.")).toList()).build()).build();
+                answers.add(dom);
+            });
+
+            ctx.response(
+                    DnsMessageDom
+                            .builder()
+                            .header(
+                                    DnsHeaderDom
+                                            .builder()
+                                            .id(ctx.query().header().id())
+                                            .build()
+                            )
+                            .answers(answers)
+                            .build());
+        });
+
     }
 }
--- app/src/main/java/io/hedges/zoned/app/Main.java
+++ app/src/main/java/io/hedges/zoned/app/Main.java
@@ -3,7 +3,6 @@ package io.hedges.zoned.app;
 import io.hedges.zoned.core.DnsImplementationProvider;
 import io.hedges.zoned.core.DnsServer;
 import io.hedges.zoned.netty.NettyDnsImplementationProvider;
-import io.hedges.zoned.netty.UdpNettyDnsServer;
 
 import java.net.InetSocketAddress;
 
@@ -13,7 +12,7 @@ public final class Main {
         int port = args.length > 0 ? Integer.parseInt(args[0]) : 53;
         DnsImplementationProvider dip = new NettyDnsImplementationProvider(53, upstream);
         DnsServer server = dip.server();
-        server.requestHandler(new DefaultDnsRequestRouter()).start();
+        server.requestHandler(new DefaultDnsRequestRouter(dip.client())).start();
         Runtime.getRuntime().addShutdownHook(new Thread(server::stop));
         System.out.println("zoned DNS server listening on UDP " + port);
     }
