843e86f4a97d961329ee96c2bf0652f576511f5e
Andy Hedges <andyhedges@users.noreply.github.com>
2025-12-18 17:57:01 +0000

    chore: removed unused imports (#14)

--- core/src/main/java/io/hedges/zoned/core/dom/rdata/CnameRecordDataDom.java
+++ core/src/main/java/io/hedges/zoned/core/dom/rdata/CnameRecordDataDom.java
@@ -6,10 +6,6 @@ import lombok.Builder;
 import lombok.Getter;
 import lombok.ToString;
 
-import java.nio.charset.StandardCharsets;
-import java.util.ArrayList;
-import java.util.List;
-
 @Getter
 @Builder
 @ToString
