4a80ad8dae966df3f072c67f1ebedde88dd5560c
Andy Hedges <andy@hedges.net>
2025-12-19 16:54:08 +0000

    test: first real test

--- core/src/main/java/io/hedges/zoned/core/DnsServer.java
+++ core/src/main/java/io/hedges/zoned/core/DnsServer.java
@@ -2,6 +2,6 @@ package io.hedges.zoned.core;
 
 public interface DnsServer {
     DnsServer requestHandler(DnsRequestHandler handler);
-    void start() throws Exception;
+    void start() throws DnsServerStartException;
     void stop();
 }
