71a14d5612ebe1233f25d77d3896aed7a4b2b321
Andy Hedges <andy@hedges.net>
2025-12-28 19:56:17 +0000

    doc: added SPDX header to java files

--- core/src/main/java/io/hedges/zoned/core/dom/rdata/OptRecordDataDom.java
+++ core/src/main/java/io/hedges/zoned/core/dom/rdata/OptRecordDataDom.java
@@ -1,3 +1,4 @@
+// SPDX-License-Identifier: Apache-2.0
 package io.hedges.zoned.core.dom.rdata;
 
 import io.hedges.zoned.core.dom.RDataDom;
--- core/src/main/java/io/hedges/zoned/core/dom/rdata/edns/CookieOptionDom.java
+++ core/src/main/java/io/hedges/zoned/core/dom/rdata/edns/CookieOptionDom.java
@@ -1,3 +1,4 @@
+// SPDX-License-Identifier: Apache-2.0
 package io.hedges.zoned.core.dom.rdata.edns;
 
 public class CookieOptionDom implements EdnsOptionDom {
