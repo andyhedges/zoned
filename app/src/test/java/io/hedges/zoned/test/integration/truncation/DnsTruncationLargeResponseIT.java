9b25ac49271f7ef6900b77e0ab2d05e62a06436f
Andy Hedges <andy@hedges.net>
2026-01-02 16:03:58 +0000

    fix: removed unused import

--- app/src/test/java/io/hedges/zoned/test/integration/truncation/DnsTruncationLargeResponseIT.java
+++ app/src/test/java/io/hedges/zoned/test/integration/truncation/DnsTruncationLargeResponseIT.java
@@ -16,7 +16,6 @@ import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Random;
-import java.util.UUID;
 
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.junit.jupiter.api.Assertions.assertTrue;
