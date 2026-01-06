9415f50c660ae5813f90b77de417e0909b33e0cd
Andy Hedges <andy@hedges.net>
2026-01-06 12:57:03 +0000

    chore: fixing checkstyle

--- netty/src/main/java/io/hedges/zoned/netty/TcpDnsHandler.java
+++ netty/src/main/java/io/hedges/zoned/netty/TcpDnsHandler.java
@@ -11,7 +11,6 @@ import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 
 import java.net.InetSocketAddress;
-import java.time.Instant;
 
 @Slf4j
 @AllArgsConstructor
--- netty/src/main/java/io/hedges/zoned/netty/UdpDnsHandler.java
+++ netty/src/main/java/io/hedges/zoned/netty/UdpDnsHandler.java
@@ -9,8 +9,6 @@ import io.netty.channel.SimpleChannelInboundHandler;
 import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 
-import java.time.Instant;
-
 @Slf4j
 @AllArgsConstructor
 public final class UdpDnsHandler extends SimpleChannelInboundHandler<UdpDnsInbound> {
